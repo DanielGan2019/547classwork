3ca2cf733e3318c8a520c48fe125b3a70ddcb196
Daniel Gan <dcg23@duke.edu>
2023-01-27 12:54:42 -0500

    added some modular components to code

--- dosing.py
+++ dosing.py
@@ -5,7 +5,6 @@
              type of medical treatment or diagnostics.
 """
 
-
 def dose_amount():
     print("Day One Dosing Guidelines")
     print("")
@@ -15,6 +14,14 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    driver()
+
+
+def driver():
+    dose_calculation(patient_weight(),
+    output(
+
+def patient_weight():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -24,13 +31,19 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
-    dosages_mg_per_kg = [30, 10, 10, 12]
-    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    return weight
+
+def dose_calculation(weight, dosage_mg_per_kg):
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def output(weight,dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
-    print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+    print("  the correct dosage is {:.1f} mg the first day".format(dosage_mg_first_day))
+
+    dosages_mg_per_kg = [30, 10, 10, 12]
+    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
 
 
 if __name__ == '__main__':
