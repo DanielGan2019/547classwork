3240a45137cbbbd260c35e9f0a162cb5cb68655e
Daniel Gan <dcg23@duke.edu>
2023-02-01 15:47:39 -0500

    Can create a new patient entry and add to list database

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
+'''
 print("This is the database.py file")
+
 print("Python thinks this is called {}".format(__name__))
 
 
@@ -12,5 +14,19 @@ HDL_analysis = bc.HDL_analysis(HDL)
 print("The HDL analysis is {}".format(HDL_analysis))
 
 bc.generic_input("Other Test")
+'''
 
+def create_patient_entry(patient_name, patient_mrn, patient_age):
+    new_patient = [patient_name, patient_mrn, patient_age]
+    return new_patient
 
+def main_driver():
+    db = []
+    db.append(create_patient_entry("Ann Ables", 1, 34))
+    db.append(create_patient_entry("Bob Boyles", 2, 45))
+    db.append(create_patient_entry("Chris Chou", 3, 52))
+    print(db)
+    
+
+if __name__ == "__main__":
+    main_driver()
