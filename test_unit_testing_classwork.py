50835a6fbf3e282cfc591e91ac33ad06eaeb3908
Daniel Gan <dcg23@duke.edu>
2023-02-17 15:40:28 -0500

    Developed function to determine the y value of a point on the line between two given points

--- test_unit_testing_classwork.py
+++ test_unit_testing_classwork.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_y_value(t1, t2, x_input, expected):
+def test_y_value(p1, p2, x_input, expected):
     from unit_testing_classwork import y_value
-    answer = y_value(t1, t2, x_input)
+    answer = y_value(p1, p2, x_input)
     assert answer == expected
