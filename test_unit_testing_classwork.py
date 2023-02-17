097e8e39592380b7897150b2a9899ba063d411f6
Daniel Gan <dcg23@duke.edu>
2023-02-17 16:24:17 -0500

    PEP8 changes and unit test changes

--- test_unit_testing_classwork.py
+++ test_unit_testing_classwork.py
@@ -7,7 +7,7 @@ import pytest
                           ((-2, 1), (3, 3), 1, 2.2),
                           ((-2, 1), (3, 3), 4, 3.4)])
 def test_y_value(p1, p2, x_input, expected):
-    from unit_test_classwork import y_value
+    from unit_testing_classwork import y_value
     answer = y_value(p1, p2, x_input)
     assert answer == expected
 
@@ -17,7 +17,7 @@ def test_y_value(p1, p2, x_input, expected):
                           ((1.0, 1), (3.0, 3), (1.0, 3.0)),
                           ((-2, 1), (3, 3), (-2, 3))])
 def test_get_x(p1, p2, expected):
-    from unit_test_classwork import get_x
+    from unit_testing_classwork import get_x
     answer = get_x(p1, p2)
     assert answer == expected
 
@@ -27,7 +27,7 @@ def test_get_x(p1, p2, expected):
                           ((1.0, 1.0), (3.0, 3.0), (1.0, 3.0)),
                           ((-2, -2), (3, 3), (-2, 3))])
 def test_get_y(p1, p2, expected):
-    from unit_test_classwork import get_y
+    from unit_testing_classwork import get_y
     answer = get_y(p1, p2)
     assert answer == expected
 
@@ -37,7 +37,7 @@ def test_get_y(p1, p2, expected):
                           ((1.0, 1), (3.0, 3), 1),
                           ((-2, 1), (3, 3), 0.4)])
 def test_find_slope(p1, p2, expected):
-    from unit_test_classwork import find_slope
+    from unit_testing_classwork import find_slope
     answer = find_slope(p1, p2)
     assert answer == expected
 
@@ -47,6 +47,6 @@ def test_find_slope(p1, p2, expected):
                           ((1.0, 1), (3.0, 3), 0),
                           ((-2, 1), (3, 3), 1.8)])
 def test_find_intercept(p1, p2, expected):
-    from unit_test_classwork import find_intercept
+    from unit_testing_classwork import find_intercept
     answer = find_intercept(p1, p2)
     assert answer == expected
