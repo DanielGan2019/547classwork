7fde2b2f4888eb0ec47c00a2fb36f592cedb3574
Daniel Gan <dcg23@duke.edu>
2023-03-03 13:25:22 -0500

    March 3rd classwork on servers

---
+++ health_db_server.py
@@ -0,0 +1,11 @@
+'''
+database = dictionary
+keys = ids for the patients
+value: int
+
+{1: {"id": 1, "name": "Daivid", "blood_type": O+},
+ 2: {"id": 1, "name": "Daivid", "blood_type": O+},
+ 3: {"id": 1, "name": "Daivid", "blood_type": O+}}
+
+'''
+
