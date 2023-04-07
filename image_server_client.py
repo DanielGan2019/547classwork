79d161e810cf2fd1ba09ab056d48da5f6b463562
Daniel Gan <dcg23@duke.edu>
2023-04-07 13:11:20 -0400

    in class work for converting images

--- image_server_client.py
+++ image_server_client.py
@@ -1,10 +1,15 @@
 from tkinter import filedialog
+import base64
+import requests
+
+server = "http://vcm-21170.vm.duke.edu"
 
 # Select image to upload
-
 def select_image():
-    filename = filedialog.askopenfilename(initialdir="Images")
+    # filename = filedialog.askopenfilename(initialdir="Images")
+    filename = "racoon.jpg"
     return filename
+
 # Convert style to b64 string
 def convert_image_file_to_base64_string(filename):
     with open(filename, "rb") as image_file:
@@ -12,8 +17,16 @@ def convert_image_file_to_base64_string(filename):
     b64_string = str(b64_bytes, encoding='utf-8')
     return b64_string
 # Upload b64 string to server
+def upload_to_server(b64_image, net_id, id_no):
+    new_result = {"image": b64_image, "net_id": net_id, "id_no": id_no}
+    r = requests.post(server + "/add_image", json=new_result)
+    print(r.text)
+
 
 # Download watermarked image
+def get_watermark_image(net_id, id_no):
+    r = requests.get(server + "/get_image/{}/{}".format(net_id,id_no))
+    print(r.text)
 
 
 def main():
@@ -22,3 +35,8 @@ def main():
         return
     b64_image = convert_image_file_to_base64_string(filename)
     print(b64_image)
+    upload_to_server(b64_image, "dcg23", 69)
+    get_watermark_image('dcg23', 69)
+
+if __name__ == "__main__":
+    main()
